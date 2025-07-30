6bb9e794261c36dca9a15c021b36a409433cd1ab
Adil Ramzan <adilramzan@carecloud.com>
2025-07-30 16:35:41 +0500

    Update SafariView.swift
    Changes in Detail View and Safari View to open URL

--- AppleFrameworks/SafariView.swift
+++ AppleFrameworks/SafariView.swift
@@ -5,4 +5,17 @@
 //  Created by ADIL RAMZAN on 30/07/2025.
 //
 
-import Foundation
+import SwiftUI
+import SafariServices
+
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
